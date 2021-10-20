ae386b5515a9e1dd1f660e44b87631a013f90015
Christian Medders <cmedders@cmedders-l1.attlocal.net>
2021-10-20 15:53:41 -0400

    init cypress development with updated test cases

--- cypress/integration/e2e_tests/dom-snapshot-test.spec.js
+++ cypress/integration/e2e_tests/dom-snapshot-test.spec.js
@@ -18,28 +18,8 @@
           .map((el) => el.innerText);
       });
   });
-  // describe('text snapshot', () => {
-  //   it("toMatchSnapshopt", () => {
-  //     // CY goes to each URL in the arry from site map 
-  //     // checks to make sure that each page successfully loads 
-  //     urls.forEach((url) => {
-  //       cy.request({
-  //         url: url,
-  //         headers: {
-  //           "Content-Type": "text/html",
-  //           accept: "*/*",
-  //           "user-agent":
-  //             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
-  //         },
-  //       }).then(() => {
-  //           cy.get('body')
-  //           .toMatchSnapshot(); 
-  //       });
-  //     });
-  //   });
-  // })
   describe('text snapshot', () => {
-    it('toMatchImageSnapshot', () => {
+    it('toMatchSnapshot', () => {
       // CY goes to each URL in the arry from site map 
       // checks to make sure that each anchor link is working
       urls.forEach((url) => {
@@ -59,3 +39,33 @@
     });
   })
 
+
+
+
+
+
+
+
+
+
+
+   // describe('text snapshot', () => {
+  //   it("toMatchSnapshopt", () => {
+  //     // CY goes to each URL in the arry from site map 
+  //     // checks to make sure that each page successfully loads 
+  //     urls.forEach((url) => {
+  //       cy.request({
+  //         url: url,
+  //         headers: {
+  //           "Content-Type": "text/html",
+  //           accept: "*/*",
+  //           "user-agent":
+  //             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+  //         },
+  //       }).then(() => {
+  //           cy.get('body')
+  //           .toMatchSnapshot(); 
+  //       });
+  //     });
+  //   });
+  // })
