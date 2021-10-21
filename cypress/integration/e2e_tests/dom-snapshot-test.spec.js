00a57f49bd7762c6caea35cd284e0de73968cec5
clmedders <christian.medders@bixal.com>
2021-10-21 12:38:48 -0400

    change target to main

--- cypress/integration/e2e_tests/dom-snapshot-test.spec.js
+++ cypress/integration/e2e_tests/dom-snapshot-test.spec.js
@@ -32,7 +32,7 @@
         //       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
         //   },
         cy.visit(url).then(() => {
-          cy.get('body')
+          cy.get('main')
           .toMatchSnapshot();
         });
       });
