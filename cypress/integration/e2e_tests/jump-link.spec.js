d17ede37dcae9f14054b1bee7223bfa9d03c6b56
clmedders <christian.medders@bixal.com>
2021-10-26 16:36:05 -0400

    cleaned up files from extra unused code

--- cypress/integration/e2e_tests/jump-link.spec.js
+++ cypress/integration/e2e_tests/jump-link.spec.js
@@ -29,23 +29,6 @@
       })
     })
   })
-  
-  
-  
-  
-  
-  
-  
-  
-  // describe('check anchor link on homepage', () => {
-  //   before(('visit homepage', () => {
-  //     cy.visit('http://localhost:4000')
-  //   }))
-  //   it('check anchor link', () => {
-  //     cy.get('[href="/#gov-business"]').click({force: true})
-  //     cy.url().should('include', '#gov-business')
-  //   })
-  // })
 
 
 
@@ -67,22 +50,6 @@
 
 
 
-   // describe('check link', () => {
-    //     it('should navigate to ... when link is clicked', () => {
-    //       const { getByText } = render(<a href="/#gov-business">Doing Business with the U.S. Government</a>);
-        
-    //       const link = getByText('Doing Business with the U.S. Government');
-        
-    //       fireEvent.click(link);
-        
-    //       expect(window.location.href).toBe('http://localhost:4000/#gov-business');
-    //     });
-    // })
-    // it("has anchor tags", () => {
-    //   cy.visit('http://localhost:4000/#gov-business');
-    //   cy.contains("a", "#")
-    //     .scrollIntoView()
-    //     .should("not.have.attr", "href", "#undefined");
-    // });
+
 
   
