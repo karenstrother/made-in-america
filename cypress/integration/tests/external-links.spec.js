61fd19ee7e1c1ac126b78bf5d39f1ce00b995976
clmedders <christian.medders@bixal.com>
2021-11-04 17:01:50 -0400

    moved specs to 'test' folder to break tests up

---
+++ cypress/integration/tests/external-links.spec.js
@@ -0,0 +1,31 @@
+  /// <reference types="Cypress" />
+let urls = [];
+before(() => {
+  cy.request({
+    url:'http://localhost:4000/sitemap.xml',
+    headers: {
+      "Content-Type": "text/xml; charset=utf-8",
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+    },
+  })
+    .as("sitemap")
+    .then((response) => {
+      urls = Cypress.$(response.body)
+        .find("loc")
+        .toArray()
+        .map((el) => el.innerText);
+    });
+});
+describe('check external links', () => {
+  it("check external links", () => {
+    // CY goes to each URL in the arry from site map 
+    // checks to make sure that each anchor link is working
+    urls.forEach((url) => {
+      cy.visit(url).then(() => {
+          cy.get('.usa-link--external')
+          .should('have.attr', 'target', '_blank')
+      });
+    });
+  });
+})
---
+++ cypress/integration/tests/jump-link.spec.js
@@ -0,0 +1,31 @@
+  /// <reference types="Cypress" />
+  let urls = [];
+  before(() => {
+    cy.request({
+      url:'http://localhost:4000/sitemap.xml',
+      headers: {
+        "Content-Type": "text/xml; charset=utf-8",
+        "user-agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+      },
+    })
+      .as("sitemap")
+      .then((response) => {
+        urls = Cypress.$(response.body)
+          .find("loc")
+          .toArray()
+          .map((el) => el.innerText);
+      });
+  });
+  describe('check anchor links on site', () => {
+    it('anchor links', () => {
+      urls.forEach((url) => {
+        cy.visit(url).then(() => {
+          cy.get("a").each($a => {
+            const message = $a.text();
+            expect($a, message).to.have.attr("href").not.contain("undefined");
+          });
+        })
+      })
+    })
+  })
