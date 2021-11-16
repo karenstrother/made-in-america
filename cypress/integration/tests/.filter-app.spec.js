0d4a3efa19e95c9884378e29aa844e7af1b60527
clmedders <christian.medders@bixal.com>
2021-11-16 11:34:09 -0500

    not needed any more

--- cypress/integration/tests/.filter-app.spec.js
+++
@@ -1,6 +0,0 @@
-  /// <reference types="Cypress" />
-  describe('check app is filtering correctly', () => {
-    it('filter app', () => {
-      cy.visit('http://localhost:4000/waivers/')
-    })
-  })
