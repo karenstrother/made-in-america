164c26d330d4029b3850066e49acd36747c80dcc
clmedders <christian.medders@bixal.com>
2021-12-13 12:20:35 -0500

    udpated filter test and HTML with data-test tag

--- cypress/integration/waiver-app_tests/filter-app.spec.js
+++ cypress/integration/waiver-app_tests/filter-app.spec.js
@@ -1,14 +1,16 @@
  /// <reference types="Cypress" /> 
- describe('filter by status', () => {
-  it('UI changes when filter is selected', () => {
-    cy.visit('https://www.madeinamerica.gov/waivers/')
-    cy.get('[name="filtering"]')
-    .select('Reviewed')
-    .get('[class="margin-0 text-normal"]')
-    .contains('Reviewed').should('be.visible')
-    cy.get('[name="filtering"]')
-    .select('Submitted')
-    .get('[class="margin-0 text-normal"]')
-    .contains('Submitted').should('be.visible')
+describe('filter by status', () => {
+it('UI changes when filter is selected', () => {
+  cy.visit('localhost:4000/waivers')
+  cy.get('[data-test="filter-select"] select').children().as('filter-options')
+  cy.get('@filter-options').then((filterOptions) => {
+  filterOptions.each(option => {
+  if(filterOptions[option].value !== 'all') {
+    cy.get('[data-test="filter-select"] select')
+    .select(filterOptions[option].value)
+    .should('have.value', filterOptions[option].value)
+      } 
+    })
   })
+})
 })
