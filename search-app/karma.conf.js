b28220fc3a32a065121f38e55407f8afadcdd33c
gscottqueen <scott.queen@bixal.com>
2021-12-14 16:52:47 -0500

    currently only running karma on pipeline

--- search-app/karma.conf.js
+++ search-app/karma.conf.js
@@ -24,9 +24,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: false,
-    restartOnFileChange: true
+    singleRun: true,
   });
 };
