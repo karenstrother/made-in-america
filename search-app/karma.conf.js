a3767ae845fe78b904a5a24dc09de73e3bda0142
gscottqueen <scott.queen@bixal.com>
2022-01-03 13:26:35 -0500

    convert to esmodule

--- search-app/karma.conf.js
+++ search-app/karma.conf.js
@@ -1,22 +1,24 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+import * as path from 'path'
+
+const karmaConfig = config => {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-jasmine-html-reporter',
+      'karma-coverage-istanbul-reporter',
+      '@angular-devkit/build-angular/plugins/karma',
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/usa-components'),
+      dir: path.join(__dirname, './coverage/usa-components'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
@@ -28,3 +30,5 @@ module.exports = function (config) {
     singleRun: true,
   })
 }
+
+export default karmaConfig()
