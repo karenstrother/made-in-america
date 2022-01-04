53288571330561314e19019dfd376231fe6df4e1
clmedders <christian.medders@bixal.com>
2022-01-04 15:56:14 -0500

    latest updated to .spec to cover sorting function

--- cypress/integration/tests/waiver-app/sort-results.spec.js
+++ cypress/integration/tests/waiver-app/sort-results.spec.js
@@ -1,55 +1,36 @@
 /// <reference types="Cypress" />
-describe('sort by alphabetical', () => {
+describe('sort waivers by alphabetical order', () => {
   before('visit site', () => {
     cy.visit('localhost:4000/waivers')
   })
   beforeEach('create alias', () => {
     cy.get('[data-test="sort-select"] select').children().as('sort-options')
-    cy.get('[data-test="procurement-title"]').as('title')
+    cy.get('[data-test="procurement-title"]')
+      .then(elements => {
+        const titles = elements.map((element, i) => {
+          return elements[element].innerText
+        })
+        return titles
+      })
+      .as('title')
+    cy.get('@title')
+      .then(titles => {
+        titles.sort()
+      })
+      .as('alphaSort')
   })
-  // it('waivers are alphabetical', () => {
-  //   cy.get('@sort-options').then((sortOptions) => {
-  //   sortOptions.each(option => {
-  //   if(sortOptions[option].value !== 'recent') {
-  //   cy.get('[data-test="sort-select"] select')
-  //   .select(sortOptions[option].value)
-  //   cy.get('[data-test="procurement-title"]').then(() => {
-  //   expect(sortOptions).to.deep.equal(sortOptions.sort())
-  // })
-  // }
-  // })
-  // })
-  // })
-
-  it('waivers are alphabetical', () => {
+  it('waivers are in alphabetical order after being sorted', () => {
     cy.get('@sort-options').then(sortOptions => {
       sortOptions.each(option => {
         if (sortOptions[option].value !== 'recent') {
           cy.get('[data-test="sort-select"] select').select(
             sortOptions[option].value
           )
-          cy.get('[data-test="procurement-title"]')
-            .invoke('text')
-            .each(title => {
-              expect(title).to.deep.equal(sortOptions.sort())
-            })
-          // .then((text) => {
-          //   expect(text).to.deep.equal(sortOptions.sort())
-          // })
-
-          // const innerText = (el) => el.innerText
-
-          // .then((title) => {
-          //   cy.get(title)
-          //   .each(title => {
-          //     title.innerText
-          //   })
-          // })
+          cy.get('@title').then(titles => {
+            expect(titles).to.equal(titles.sort())
+          })
         }
       })
     })
   })
 })
-
-// need to get the inner text and then put that into an array
-// and then check that is in alpha order
