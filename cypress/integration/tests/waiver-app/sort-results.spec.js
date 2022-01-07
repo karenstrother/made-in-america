ad803fba41d5928400d46d0050ee04e5ebdd538b
clmedders <christian.medders@bixal.com>
2022-01-07 16:46:47 -0500

    check point... this should be the working test

--- cypress/integration/tests/waiver-app/sort-results.spec.js
+++ cypress/integration/tests/waiver-app/sort-results.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="Cypress" />
+
 describe('sort waivers by alphabetical order', () => {
   before('visit site', () => {
     cy.visit('localhost:4000/waivers')
@@ -17,34 +18,27 @@ describe('sort waivers by alphabetical order', () => {
   it('waivers are in alphabetical order after being sorted', () => {
     cy.get('@sort-options').then(sortOptions => {
       sortOptions.each(option => {
-        // if (sortOptions[option].value !== 'recent') {
-        cy.get('[data-test="sort-select"] select').select(
-          sortOptions[option].value
-        )
+        if (sortOptions[option].value !== 'recent') {
+          cy.get('[data-test="sort-select"] select').select(
+            sortOptions[option].value
+          )
 
-        // cy.get('@titles').then(titles => {
-        //   console.log('og titles', titles)
+          // cy.get('@titles').then(titles => {
+          //   console.log('og titles', titles)
 
-        cy.get('[data-test="procurement-title"]').each(elements => {
-          const t = elements.map(elementIndex => {
-            return elements[elementIndex].innerText
+          cy.get('[data-test="procurement-title"]').each(elements => {
+            const t = elements.map(elementIndex => {
+              return elements[elementIndex].innerText
+            })
+            expect(t).to.eq(t.sort())
           })
-          // console.log('t', t.sort())
-          expect(t).to.eq(t.sort())
 
-          // console.log("t",t)
+          // console.log('titles', sortedTitles)
+          // console.log('sorted', titles.sort())
+          // console.log('titles.2', titlesArray)
 
-          // console.log("t sort", t.sort())
-
-          // return t
-        })
-
-        // console.log('titles', sortedTitles)
-        // console.log('sorted', titles.sort())
-        // console.log('titles.2', titlesArray)
-
-        // })
-        // }
+          // })
+        }
       })
     })
   })
