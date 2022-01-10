7fbbad213a19b754dfbfa41062b89eb2ec201a4c
clmedders <christian.medders@bixal.com>
2022-01-10 13:32:48 -0500

    have cypress skipping waiver page

--- cypress/integration/tests/non-availability-check.spec.js
+++ cypress/integration/tests/non-availability-check.spec.js
@@ -3,9 +3,11 @@ describe('non-availability check', () => {
   it('non-availability should not be present on site', () => {
     cy.getUrlsArray().then($urls => {
       $urls.forEach(url => {
-        cy.visit(url)
-        cy.contains('non-availability').should('not.exist')
-        cy.contains('Non-availability').should('not.exist')
+        if (url !== 'http://localhost:4000/waivers/') {
+          cy.visit(url)
+          cy.contains('non-availability').should('not.exist')
+          cy.contains('Non-availability').should('not.exist')
+        }
       })
     })
   })
