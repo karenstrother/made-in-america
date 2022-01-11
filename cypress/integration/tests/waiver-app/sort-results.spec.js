277c36aeac02d678edddb3fe8a2b40f77a58d96b
clmedders <christian.medders@bixal.com>
2022-01-11 17:02:45 -0500

    checkpoint for file

--- cypress/integration/tests/waiver-app/sort-results.spec.js
+++ cypress/integration/tests/waiver-app/sort-results.spec.js
@@ -18,33 +18,21 @@ describe('sort waivers by alphabetical order', () => {
   it('waivers are in alphabetical order after being sorted', () => {
     cy.get('@sort-options').then(sortOptions => {
       sortOptions.each(option => {
-        // if (sortOptions[option].value !== 'recent') {
-        cy.get('[data-test="sort-select"] select').select(
-          sortOptions[option].value
-        )
-
-        // cy.get('@titles').then(titles => {
-        //   console.log('og titles', titles)
-
-        cy.get('[data-test="procurement-title"]').each(elements => {
-          const t = elements.map(elementIndex => {
-            return elements[elementIndex].innerText
+        if (sortOptions[option].value !== 'recent') {
+          cy.get('[data-test="sort-select"] select').select(
+            sortOptions[option].value
+          )
+          cy.get('[data-test="procurement-title"]').each(elements => {
+            const t = elements.map(elementIndex => {
+              return elements[elementIndex].innerText
+            })
+            expect(t).to.eq(t.sort())
           })
-          expect(t).to.eq(t.sort())
-        })
-
-        // console.log('titles', sortedTitles)
-        // console.log('sorted', titles.sort())
-        // console.log('titles.2', titlesArray)
-
-        // })
-        // }
+        }
       })
     })
   })
 })
-
-// alphabetical
 
 //   const filetypes = ['Access', 'Excel', 'Text/CSV','PDF','JSON','dsdsd'];
 //   cy.get('.data-sourcename').should(($els) => {
