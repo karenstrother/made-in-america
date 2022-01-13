0a2be49cefad63e29ff065e67481ecaccc9e4abd
clmedders <christian.medders@bixal.com>
2022-01-12 20:51:34 -0500

    updated spec file for final test

--- cypress/integration/tests/waiver-app/sort-results.spec.js
+++ cypress/integration/tests/waiver-app/sort-results.spec.js
@@ -6,38 +6,28 @@ describe('sort waivers by alphabetical order', () => {
   })
   beforeEach('create alias', () => {
     cy.get('[data-test="sort-select"] select').children().as('sort-options')
-    cy.get('[data-test="procurement-title"]')
-      .then(elements => {
-        const titles = elements.map(elementIndex => {
-          return elements[elementIndex].innerText
-        })
-        return titles
-      })
-      .as('titles')
   })
   it('waivers are in alphabetical order after being sorted', () => {
     cy.get('@sort-options').then(sortOptions => {
       sortOptions.each(option => {
-        if (sortOptions[option].value !== 'recent') {
-          cy.get('[data-test="sort-select"] select').select(
-            sortOptions[option].value
-          )
-          cy.get('[data-test="procurement-title"]').each(elements => {
-            const t = elements.map(elementIndex => {
-              return elements[elementIndex].innerText
-            })
-            expect(t).to.eq(t.sort())
-          })
-        }
+        cy.get('[data-test="sort-select"] select').select(
+          sortOptions[option].value
+        )
+        cy.get('[data-test="procurement-title"]').then(elements => {
+          const titles = elements.toArray().map(element => element.innerText)
+          const alphabeticalTitles = titles
+            .slice()
+            .sort((a, b) => a.localeCompare(b))
+          switch (sortOptions[option].value) {
+            case 'alphabetical':
+              expect(titles).to.deep.equal(alphabeticalTitles)
+              break
+            case 'recent':
+              expect(titles).to.not.deep.equal(alphabeticalTitles)
+              break
+          }
+        })
       })
     })
   })
 })
-
-//   const filetypes = ['Access', 'Excel', 'Text/CSV','PDF','JSON','dsdsd'];
-//   cy.get('.data-sourcename').should(($els) => {
-// // map jquery elements to array of their innerText
-// const elsText = $els.toArray().map(el => el.innerText)
-// expect(elsText).to.deep.eq(filetypes)
-
-// })
