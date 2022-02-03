2ca5705288f38a6fc137748844757bf351f6bb08
clmedders <christian.medders@bixal.com>
2022-02-03 14:09:39 -0500

    udpated name and descriptions

--- cypress/integration/tests/improve-site.spec.js
+++ cypress/integration/tests/improve-site.spec.js
@@ -1,13 +1,13 @@
 /// <reference types="Cypress" />
-describe('check how to improve site button works', () => {
+describe('check that the to improve site button works', () => {
   it('should open pop up and be able to be typed in', () => {
     cy.getUrlsArray().then($urls => {
       $urls.forEach(url => {
         cy.visit(url).then(() => {
           cy.get('[class="fixed-tab-button usa-button"]').click()
           cy.get('[class="wrapper"]').should('be.visible')
-          cy.get('[name="answer_01"]').type('test')
-          cy.get('[name="answer_03"]').type('test')
+          cy.get('[id="answer_01"]').type('test')
+          cy.get('[id="answer_03"]').type('test')
           cy.get(
             '[href="https://www.gsa.gov/reference/gsa-privacy-program/privacy-act-statement-for-design-research"]'
           ).should('have.attr', 'target', '_blank')
