09591479de5e229c7f0f18852a2525f329dc3326
clmedders <christian.medders@bixal.com>
2022-02-23 17:28:30 -0500

    check point

--- cypress/integration/tests/waiver-app/status-date.spec.js
+++ cypress/integration/tests/waiver-app/status-date.spec.js
@@ -4,8 +4,8 @@ describe('check status date', () => {
     cy.visit('localhost:4000/waivers')
   })
   it('check that date is present and formatted correctly', () => {
-    cy.get('[data-test="status-date"]')
-      .should('be.visible')
-      .and('match', 'MM/dd/yyyy')
+    cy.get('[data-test="status-date"]').should('be.visible')
+    // .and('match', 'MM/dd/yyyy')
+    cy.get('[data-test="status-date"]').and('match', /(\d2)(\d2)(\d4)/)
   })
 })
