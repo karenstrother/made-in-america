965df9b7cf2be03973f9794cc874ff6999bf7713
clmedders <christian.medders@bixal.com>
2022-03-02 10:56:25 -0500

    updated test with correct regex and invoke html

--- cypress/integration/tests/waiver-app/status-date.spec.js
+++ cypress/integration/tests/waiver-app/status-date.spec.js
@@ -5,7 +5,9 @@ describe('check status date', () => {
   })
   it('check that date is present and formatted correctly', () => {
     cy.get('[data-test="status-date"]').should('be.visible')
-    // .and('match', 'MM/dd/yyyy')
-    cy.get('[data-test="status-date"]').and('match', /(\d2)(\d2)(\d4)/)
+    cy.get('[data-test="status-date"]')
+      .first()
+      .invoke('html')
+      .should('match', /(0|1)[0-9]\/[0-3][0-9]\/(20)[0-9]{2}/)
   })
 })
