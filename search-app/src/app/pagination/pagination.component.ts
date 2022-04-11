badc63dafd3474e8af38cef41ee69e28811ae054
kedwards-a <kedwards-a@kedwards-l1.fios-router.home>
2022-04-11 15:19:14 -0400

    pagination bug fix

--- search-app/src/app/pagination/pagination.component.ts
+++ search-app/src/app/pagination/pagination.component.ts
@@ -14,22 +14,28 @@ export class PaginationComponent implements OnInit {
 
   renderedButtons: number[]
 
-  static handleButtonValues = buttonValues => {
+  static handleButtonValues = (buttonValues, updatedButtons) => {
     if (buttonValues > 3) {
-      return [2, 3]
+      return updatedButtons
     }
     if (buttonValues > 2) {
-      return [2]
+      return updatedButtons
     }
     return []
   }
 
   ngOnInit(): void {
-    this.renderedButtons = PaginationComponent.handleButtonValues(this.last)
+    this.renderedButtons = PaginationComponent.handleButtonValues(
+      this.last,
+      [2, 3]
+    )
   }
 
   ngDoCheck() {
-    this.renderedButtons = PaginationComponent.handleButtonValues(this.last)
+    this.renderedButtons = PaginationComponent.handleButtonValues(
+      this.last,
+      this.renderedButtons
+    )
   }
 
   goTo(index) {
@@ -37,10 +43,12 @@ export class PaginationComponent implements OnInit {
     window.scroll(0, 0)
 
     if ([1, 2].includes(this.current)) {
-      this.renderedButtons = PaginationComponent.handleButtonValues(this.last)
+      this.renderedButtons = PaginationComponent.handleButtonValues(this.last, [
+        2,
+      ])
     } else if ([this.last, this.last - 1].includes(this.current)) {
       this.renderedButtons =
-        this.last > 3 ? [this.last - 2, this.last - 1] : [2]
+        this.last > 3 ? [this.last - 3, this.last - 1] : [2]
     } else {
       this.renderedButtons = [this.current - 1, this.current, this.current + 1]
     }
